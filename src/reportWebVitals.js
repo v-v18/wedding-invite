66883f16023a7a8d2325172e36b8f08030ca085f
Nguyen <Viet.Nguyen@sva.de>
2025-02-03 22:51:56 +0100

    add site perf measure

---
+++ src/reportWebVitals.js
@@ -0,0 +1,8 @@
+// src/reportWebVitals.js
+
+const reportWebVitals = (metric) => {
+    console.log(metric); // You can log or send the metric data to your analytics tool
+  };
+  
+  export default reportWebVitals;
+  
